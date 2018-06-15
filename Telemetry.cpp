6545ee548bb969805ba3356ad9962856374152d4
Khalil Estel <khalil.estell@sjsu.edu>
2018-06-14 18:54:42 -0700

    Addin' demo tele cpps
    and new file is needed for tele to do its thing!

---
+++ Telemetry.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "telemetry.hpp"
+
+char types[32];
+void * array[32];
+int index = 0;
+
+void registerVariable(char type, void * data) {}
+void task1() {}
+void task1() {}
+void remote_call() {}
